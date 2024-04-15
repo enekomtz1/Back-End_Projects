3bc2157021c22b9ddf79d885f4ba16044cb33592
enekomtz1 <enekomartinezm@gmail.com>
2024-04-15 11:13:07 +0200

    Finished code

--- 02-TaskManager_API/middlewares/auth.js
+++ 02-TaskManager_API/middlewares/auth.js
@@ -4,7 +4,6 @@ const User = require("../models/User");
 const auth = async (req, res, next) => {
 	try {
 		const token = req.header("Authorization").replace("Bearer ", "");
-		// dasfasgasgsagfasg
 		const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
 		const user = await User.findOne({
 			_id: decoded._id,
